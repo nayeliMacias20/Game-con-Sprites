028af2b4f41a132e2a3c4bef440e90577282164c
Nayeli Macias <69863450+nayeliMacias20@users.noreply.github.com>
2022-12-05 01:07:17 -0600

    second commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
-var tecla = 0;
+//var tecla = 0;
 const player = new Image();
 //playerImage.src ='shadow_dog.png';
 player.src = 'assets/shadow_dog.png';
@@ -16,49 +16,66 @@ const spriteWidth = 575;
 const spriteHeight = 523;
 
 window.addEventListener('keydown', e => {
-    if (e.key == 'ArrowUp') {
-        frameY++;
-        if (frameY > 9) {
-            frameY = 0;
-            animateTwo();
-            animate();
-        } else {
-            animateTwo();
-            animate();
-        }
-    }
-    if (e.key == 'ArrowDown') {
-        frameY--;
-        if (frameY < 0) {
-            frameY = 0;
-            animateTwo();
-            animate();
-        } else {
-            animateTwo();
-            animate();
-        }
-    }
+    console.log(e.key);
+    animacion(e.key);
 })
-//Funcion animacion2 con las repeticiones
-function animateTwo() {
-    if (frameY == 0) repetitions = 6;
-    else if (frameY == 1) repetitions = 6;
-    else if (frameY == 2) repetitions = 6;
-    else if (frameY == 3) repetitions = 8;
-    else if (frameY == 4) repetitions = 10;
-    else if (frameY == 5) repetitions = 4;
-    else if (frameY == 6) repetitions = 6;
-    else if (frameY == 7) repetitions = 6;
-    else if (frameY == 8) repetitions = 11;
-    else if (frameY == 9) repetitions = 3;
+//Funcion animacion con las repeticiones
+function animacion(key) {
+    switch (key.toLowerCase()) {
+        case 'a':
+            frameY = 1;
+            repetitions = 6;
+            break;
+        case 'b':
+            frameY = 2;
+            repetitions = 6;
+            break;
+        case 'c':
+            frameY = 3;
+            repetitions = 8;
+            break;
+        case 'd':
+            frameY = 4;
+            repetitions = 8;
+            break;
+        case 'e':
+            frameY = 5;
+            repetitions = 3;
+            break;
+        case 'f':
+            frameY = 6;
+            repetitions = 5;
+            break;
+        case 'g':
+            frameY = 7;
+            repetitions = 6;
+            break;
+        case 'h':
+            frameY = 8;
+            repetitions = 11;
+            break;
+        case 'i':
+            frameY = 9;
+            repetitions = 3;
+            break;
+        case 'j':
+            frameY = 0;
+            repetitions = 6;
+            break;
+
+        default:
+            alert('Ingrese la tecla correcta :)');
+            break;
+    }
 }
 //Funcion animacion1
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     //Mostraremos la imagen canvas1
-    ctx.drawImage(player, frameX * spriteWidth, frameY * spriteHeight,
-        spriteWidth, spriteHeight,
-        0, 0, spriteWidth, spriteHeight);
+    ctx.drawImage(player, 
+                  frameX * spriteWidth, frameY * spriteHeight,
+                  spriteWidth, spriteHeight,
+                  0, 0, spriteWidth, spriteHeight);
 
     if (frameX < repetitions) frameX++
     else frameX = 0;
